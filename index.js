6bd81f80c5579a26ef2445df3a7f311df97f0cf1
atulchougale <atulchougale96@gmail.com>
2023-01-29 23:03:09 +0530

    express

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-
+const express = require('express');
 const fs = require('fs')
-const http = require("http")
-const {type} = require("os")
-const port = 7000;
 
-const Server = http.createServer((req,res)=>{
+const app = express();
+
+
+
+app.get("/",(req,res)=>{
+  
     let date = new Date()
     let timeStamp = date.getTime()
     fs.writeFileSync(`${timeStamp}.txt`, `${date}` ,'utf-8')
     let data = fs.readFileSync(`${timeStamp}.txt`,"utf-8")
     res.writeHead(200,{'content-Type':'text/html'})
     res.end(data)
+   
+
 })
 
+const port = process.env.PORT || 7000;
 
-
-
-Server.listen(port,()=> console.log("listening on port "+port));
+app.listen(port,()=> console.log("listening on port"+port));
